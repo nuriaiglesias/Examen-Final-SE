219430ec4c30610241909fecd86b1a3f38d20101
nuriaiglesias <72874324+nuriaiglesias@users.noreply.github.com>
2023-05-23 16:22:02 +0200

    Luces led - Ej2

--- se2223_1op/main.c
+++ se2223_1op/main.c
@@ -1,71 +1,127 @@
-/*
- * The Clear BSD License
- * Copyright (c) 2013 - 2015, Freescale Semiconductor, Inc.
- * Copyright 2016-2017 NXP
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted (subject to the limitations in the disclaimer below) provided
- *  that the following conditions are met:
- *
- * o Redistributions of source code must retain the above copyright notice, this list
- *   of conditions and the following disclaimer.
- *
- * o Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- *
- * o Neither the name of the copyright holder nor the names of its
- *   contributors may be used to endorse or promote products derived from this
- *   software without specific prior written permission.
- *
- * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS LICENSE.
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+#include "MKL46Z4.h"
 
-#include "fsl_device_registers.h"
-#include "fsl_debug_console.h"
-#include "board.h"
+// LED (RG)
+// LED_GREEN = PTD5
+// LED_RED = PTE29
 
-#include "pin_mux.h"
-/*******************************************************************************
- * Definitions
- ******************************************************************************/
+int first = 0;
+int first2 = 0;
+int sw1_state = 0;
+int sw2_state = 0;
+int sw1_count = 0;
+int sw2_count = 0;
+
+// LED_GREEN = PTD5
+void led_green_init()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;
+  PORTD->PCR[5] = PORT_PCR_MUX(1);
+  GPIOD->PDDR |= (1 << 5);
+  GPIOD->PSOR = (1 << 5);
+}
+
+// LED_RED = PTE29
+void led_red_init()
+{
+  SIM->COPC = 0;
+  SIM->SCGC5 |= SIM_SCGC5_PORTE_MASK;
+  PORTE->PCR[29] = PORT_PCR_MUX(1);
+  GPIOE->PDDR |= (1 << 29);
+  GPIOE->PSOR = (1 << 29);
+}
+
+void Green_LED_On(void)
+{
+  GPIOD->PCOR = (1u << 5);
+}
+
+void Red_LED_On(void)
+{
+  GPIOE->PCOR = (1u << 29);
+}
+
+void Green_LED_Off(void)
+{
+  GPIOD->PSOR = (1u << 5);
+}
+
+void Red_LED_Off(void)
+{
+  GPIOE->PSOR = (1u << 29);
+}
+
+void sw1_button_init(void)
+{
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[3] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1) | PORT_PCR_IRQC(10); 
+  GPIOC->PDDR &= ~(1 << 3);
+  PORTC->ISFR |= (1 << 3);
+  NVIC_SetPriority(PORTC_PORTD_IRQn, 0); 
+}
+
+void sw2_button_init(void)
+{
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;
+  PORTC->PCR[12] = PORT_PCR_MUX(1) | PORT_PCR_PE(1) | PORT_PCR_PS(1) | PORT_PCR_IRQC(10); // set port c pin 12 as GPIO, Pull enable and select
+  GPIOC->PDDR &= ~(1 << 12);
+  PORTC->ISFR |= (1 << 12);     // Detect interruption
+  NVIC_SetPriority(PORTC_PORTD_IRQn, 1);
+}
+
+int check_SW1(void)
+{ // returns 1 if SW1 is pressed and 0 if switch is not pressed
+  if ((0x00000008 & PTC->PDIR) == 0x00000008)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
+
+int check_SW2(void)
+{ // returns 1 if SW2 is pressed and 0 if switch is not pressed
+  if ((0x00001000 & PTC->PDIR) == 0x00001000)
+  {
+    return 0;
+  }
+  else
+  {
+    return 1;
+  }
+}
 
 
-/*******************************************************************************
- * Prototypes
- ******************************************************************************/
+void PORTD_Int_Handler(void){
+      if (check_SW1()) // SW1 has been pressed
+      { 
+        Green_LED_On();
+        Red_LED_Off();
+        while (check_SW1()); // Wait until the button stops being pressed
+        PORTC->PCR[3] &= ~PORT_PCR_ISF_MASK;
+      }
 
-/*******************************************************************************
- * Code
- ******************************************************************************/
-/*!
- * @brief Main function
- */
+      if (check_SW2()) // SW2 has been pressed
+      { 
+        Red_LED_On();
+        Green_LED_Off();
+        while (check_SW2());
+        PORTC->PCR[12] &= ~PORT_PCR_ISF_MASK;
+      }
+}
+
+
 int main(void)
 {
-  char ch;
+  led_green_init();
+  led_red_init();
+  sw1_button_init();
+  sw2_button_init();
+  NVIC_EnableIRQ(PORTC_PORTD_IRQn);
 
-  /* Init board hardware. */
-  BOARD_InitPins();
-  BOARD_BootClockRUN();
-  BOARD_InitDebugConsole();
+  while(1){}
 
-  PRINTF("\r\nReinicio!\r\n");
-
-  while (1)
-    {
-      ch = GETCHAR();
-      PUTCHAR(ch);
-    }
+    return 0;
 }
